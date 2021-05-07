94056e4b3cf700ddc3cccd121c99d5bd858646b3
Salvatore Trimarchi <path72@gmail.com>
2021-05-07 10:27:26 +0200

    classe active

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,8 +3,8 @@
 		<img src="{{ asset('img/logo.png') }}" alt="">
 	</div>
 	<ul class="nav txt_nav">
-		<li class="home"><a href="{{route('home-page')}}">Home</a></li>
-		<li class="products"><a href="{{route('products-page')}}">Prodotti</a></li>
-		<li class="news"><a href="{{route('news-page')}}">News</a></li>
+		<li class="{{(Request::route()->getName()=='home-page')?'active':''}}"><a href="{{route('home-page')}}">Home</a></li>
+		<li class="{{(Request::route()->getName()=='products-page')?'active':''}}"><a href="{{route('products-page')}}">Prodotti</a></li>
+		<li class="{{(Request::route()->getName()=='news-page')?'active':''}}"><a href="{{route('news-page')}}">News</a></li>
 	</ul>
 </header>
